6acbd87c1e458ef5cb8dec5e72c784fa15ff7113
CQP <cqp_saber@hotmail.com>
2017-10-18 22:53:58 -0400

    Add Environment Variables

--- application.py
+++ application.py
@@ -1,4 +1,5 @@
-import threading, tweepy, json, time
+import threading, tweepy, time
+from os import environ
 from elasticsearch import Elasticsearch
 from index import App
 from streamer import Streamer
@@ -6,20 +7,14 @@ from streamer import Streamer
 # keywords to stream
 KEYWORDS = ['apple', 'bar', 'cat', 'dog', 'eat', 'food']
 
-# file to store auth
-KEYFILE = './keys.json'
-
 # run the app.
 if __name__ == "__main__":
     # load api key and token
-    with open(KEYFILE, 'r') as f:
-        keys = json.load(f)
-        auth = tweepy.OAuthHandler(keys['twitt_api_key'],
-                                   keys['twitt_api_secret'])
-        auth.set_access_token(keys['twitt_token_key'],
-                              keys['twitt_token_secret'])
-        es = Elasticsearch([keys['es_host']])
-        # es = Elasticsearch()
+    auth = tweepy.OAuthHandler(environ['twitt_api_key'],
+                               environ['twitt_api_secret'])
+    auth.set_access_token(environ['twitt_token_key'],
+                          environ['twitt_token_secret'])
+    es = Elasticsearch([environ['es_host']])
 
     # start worker threads
     tweet_streamer, app = Streamer(auth, es, KEYWORDS), App(es)
